37095f41a3d16f40b641ea7d9b69d27bd055d6c8
Liu YaoWen <576079353@qq.com>
2022-03-11 17:00:22 +0800

    chore: 内容修改

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <p>LYW</p>
 </template>
 
 <style>
